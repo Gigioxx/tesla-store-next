8816fb05b916c62746408ea4fcfbc0d5a3dfda89
Gigiox <guillermo.casanova.b@gmail.com>
2022-10-23 02:26:41 -0300

    💄 Modified index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
+import { Typography } from '@mui/material';
 import type { NextPage } from 'next';
+import { ShopLayout } from '../components/layouts';
 
 const Home: NextPage = () => {
-  return <h1>Tesla Store</h1>;
+  return (
+    <ShopLayout
+      title={'Tesla-Shop - Home'}
+      pageDescription={'Here you can find best Tesla products'}
+    >
+      <Typography variant='h1' component='h1'>
+        Store
+      </Typography>
+      <Typography variant='h2' sx={{ mb: 1 }}>
+        All products
+      </Typography>
+    </ShopLayout>
+  );
 };
 
 export default Home;
