135153937f4ca6108e54e046c28753a66e081b16
Gigiox <guillermo.casanova.b@gmail.com>
2022-10-24 17:13:31 -0300

    ➕ Added SWR dependency

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,20 @@ import type { NextPage } from 'next';
 import { Typography } from '@mui/material';
 
 import { ShopLayout } from '../components/layouts';
-import { initialData } from '../database/products';
+
 import { ProductList } from '../components/products';
 
-const Home: NextPage = () => {
+import useSWR from 'swr';
+const fetcher = (...args: [key: string]) =>
+  fetch(...args).then((res) => res.json());
+
+const HomePage: NextPage = () => {
+  const { data, error } = useSWR('/api/products', fetcher);
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
+
+  console.log(data);
+
   return (
     <ShopLayout
       title={'Tesla-Shop - Home'}
@@ -19,9 +29,9 @@ const Home: NextPage = () => {
       </Typography>
 
       {/* Remove any type later */}
-      <ProductList products={initialData.products as any} />
+      <ProductList products={data} />
     </ShopLayout>
   );
 };
 
-export default Home;
+export default HomePage;
