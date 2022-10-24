1cb2c40d3936568d4af3a06a2abe2e3e008df416
Gigiox <guillermo.casanova.b@gmail.com>
2022-10-24 00:29:13 -0300

    ✨ Added editable property to cartList

--- components/cart/CartList.tsx
+++ components/cart/CartList.tsx
@@ -1,3 +1,4 @@
+import { FC } from 'react';
 import NextLink from 'next/link';
 
 import {
@@ -18,7 +19,11 @@ const productsInCart = [
   initialData.products[2],
 ];
 
-export const CartList = () => {
+interface Props {
+  editable?: boolean;
+}
+
+export const CartList: FC<Props> = ({ editable = false }) => {
   return (
     <>
       {productsInCart.map((product) => (
@@ -43,8 +48,12 @@ export const CartList = () => {
               <Typography variant='body1'>
                 Size: <strong>M</strong>
               </Typography>
-              {/* Conditional */}
-              <ItemCounter />
+
+              {editable ? (
+                <ItemCounter />
+              ) : (
+                <Typography variant='h5'>3 items</Typography>
+              )}
             </Box>
           </Grid>
           <Grid
@@ -55,10 +64,12 @@ export const CartList = () => {
             flexDirection='column'
           >
             <Typography variant='subtitle1'>{`$${product.price}`}</Typography>
-            {/* Editable */}
-            <Button variant='text' color='secondary'>
-              Remove
-            </Button>
+
+            {editable && (
+              <Button variant='text' color='secondary'>
+                Remove
+              </Button>
+            )}
           </Grid>
         </Grid>
       ))}
