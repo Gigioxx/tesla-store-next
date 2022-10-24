0722c15a163e024251f3d2b13fcde8d111c2b125
Gigiox <guillermo.casanova.b@gmail.com>
2022-10-24 15:58:44 -0300

    ✨ Created endpoint to get product by slug

---
+++ pages/api/products/[slug].ts
@@ -0,0 +1,38 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { db } from '../../../database';
+import { Product } from '../../../models';
+import { IProduct } from '../../../components/products/interfaces';
+
+type Data = { message: string } | IProduct;
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  switch (req.method) {
+    case 'GET':
+      return getProductsBySlug(req, res);
+    default:
+      return res.status(400).json({
+        message: 'Bad request',
+      });
+  }
+}
+
+const getProductsBySlug = async (
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) => {
+  await db.connect();
+  const { slug } = req.query;
+  const product = await Product.findOne({ slug }).lean();
+  await db.disconnect();
+
+  if (!product) {
+    return res.status(404).json({
+      message: 'Product not found',
+    });
+  }
+
+  return res.json(product);
+};
