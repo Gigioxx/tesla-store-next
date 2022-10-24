8e381154ae42b2e683c9d5d047cc187bb3579ad6
Gigiox <guillermo.casanova.b@gmail.com>
2022-10-23 23:30:17 -0300

    ✨ Created empty shopping cart page

---
+++ pages/cart/empty.tsx
@@ -0,0 +1,35 @@
+import NextLink from 'next/link';
+
+import { RemoveShoppingCartOutlined } from '@mui/icons-material';
+import { Link, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import { ShopLayout } from '../../components/layouts';
+
+const EmptyPage = () => {
+  return (
+    <ShopLayout
+      title='Empty shopping cart'
+      pageDescription='There are no items in the shopping cart'
+    >
+      <Box
+        display='flex'
+        justifyContent='center'
+        alignItems='center'
+        height='calc(100vh - 200px)'
+        sx={{ flexDirection: { xs: 'column', sm: 'row' } }}
+      >
+        <RemoveShoppingCartOutlined sx={{ fontSize: 100 }} />
+        <Box display='flex' flexDirection='column' alignItems='center'>
+          <Typography>Your shopping cart is empty.</Typography>
+          <NextLink href='/' passHref>
+            <Link typography='h4' color='secondary'>
+              Return
+            </Link>
+          </NextLink>
+        </Box>
+      </Box>
+    </ShopLayout>
+  );
+};
+
+export default EmptyPage;
