2b8b3f53b7292ce0de4f3c46a49995249eb5933c
Gigiox <guillermo.casanova.b@gmail.com>
2022-10-30 00:56:09 -0300

    💄 Replaced values for real ones

--- components/cart/OrderSummary.tsx
+++ components/cart/OrderSummary.tsx
@@ -1,34 +1,43 @@
+import { useContext } from 'react';
 import { Grid, Typography } from '@mui/material';
+import { CartContext } from '../../context';
+import { currency } from '../../utils';
 
 export const OrderSummary = () => {
+  const { numberOfItems, subTotal, total, tax } = useContext(CartContext);
+
   return (
     <Grid container>
       <Grid item xs={6}>
         <Typography># Products</Typography>
       </Grid>
       <Grid item xs={6} display='flex' justifyContent='end'>
-        <Typography>3 items</Typography>
+        <Typography>
+          {numberOfItems} {numberOfItems > 1 ? 'Items' : 'Item'}
+        </Typography>
       </Grid>
 
       <Grid item xs={6}>
         <Typography>Subtotal</Typography>
       </Grid>
       <Grid item xs={6} display='flex' justifyContent='end'>
-        <Typography>{`$${155.95}`}</Typography>
+        <Typography>{currency.format(subTotal)}</Typography>
       </Grid>
 
       <Grid item xs={6}>
-        <Typography>Taxes (19%)</Typography>
+        <Typography>
+          Taxes ({Number(process.env.NEXT_PUBLIC_TAX_RATE) * 100}%)
+        </Typography>
       </Grid>
       <Grid item xs={6} display='flex' justifyContent='end'>
-        <Typography>{`$${29.63}`}</Typography>
+        <Typography>{currency.format(tax)}</Typography>
       </Grid>
 
       <Grid item xs={6} sx={{ mt: 2 }}>
         <Typography variant='subtitle1'>Total</Typography>
       </Grid>
       <Grid item xs={6} sx={{ mt: 2 }} display='flex' justifyContent='end'>
-        <Typography variant='subtitle1'>{`$${185.58}`}</Typography>
+        <Typography variant='subtitle1'>{currency.format(total)}</Typography>
       </Grid>
     </Grid>
   );
