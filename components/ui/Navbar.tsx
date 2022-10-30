b9aac7185fe69dd04f218b4279a1cf00e7f470d1
Gigiox <guillermo.casanova.b@gmail.com>
2022-10-30 00:56:30 -0300

    ✨ Using real cart number of items in navbar

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -19,11 +19,12 @@ import {
   SearchOutlined,
   ShoppingCartOutlined,
 } from '@mui/icons-material';
-import { UiContext } from '../../context/ui/UiContext';
+import { CartContext, UiContext } from '../../context/';
 
 export const Navbar = () => {
   const { route, push } = useRouter();
   const { toggleSideMenu } = useContext(UiContext);
+  const { numberOfItems } = useContext(CartContext);
 
   const [searchTerm, setSearchTerm] = useState('');
   const [isSearchVisible, setIsSearchVisible] = useState(false);
@@ -118,7 +119,10 @@ export const Navbar = () => {
         <NextLink href='/cart' passHref>
           <Link>
             <IconButton>
-              <Badge badgeContent={2} color='secondary'>
+              <Badge
+                badgeContent={numberOfItems > 9 ? '+9' : numberOfItems}
+                color='secondary'
+              >
                 <ShoppingCartOutlined />
               </Badge>
             </IconButton>
