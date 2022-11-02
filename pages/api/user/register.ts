aea50fd2a0f696b9dd4a96e8aae02ccf8ef10a54
Gigiox <guillermo.casanova.b@gmail.com>
2022-11-01 23:16:07 -0300

    ✨ Created register endpoint

---
+++ pages/api/user/register.ts
@@ -0,0 +1,93 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import bcrypt from 'bcryptjs';
+import { db } from '../../../database';
+import { User } from '../../../models';
+import { jwt } from '../../../utils';
+
+type Data =
+  | { message: string }
+  | {
+      token: string;
+      user: {
+        email: string;
+        name: string;
+        role: string;
+      };
+    };
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  switch (req.method) {
+    case 'POST':
+      return registerUser(req, res);
+
+    default:
+      res.status(400).json({ message: 'Bad Request' });
+  }
+}
+
+const registerUser = async (
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) => {
+  const {
+    email = '',
+    password = '',
+    name = '',
+  } = req.body as { email: string; password: string; name: string };
+
+  if (password.length < 6) {
+    return res.status(400).json({
+      message: 'Password must be at least 6 characters long',
+    });
+  }
+
+  if (name.length < 2) {
+    return res.status(400).json({
+      message: 'Name must be at least 2 characters long',
+    });
+  }
+
+  //Todo: validate email format
+  //   if(email) {
+
+  //   }
+
+  await db.connect();
+  const user = await User.findOne({ email });
+
+  if (user) {
+    return res.status(400).json({ message: 'Email already registered' });
+  }
+
+  const newUser = new User({
+    email: email.toLowerCase(),
+    password: bcrypt.hashSync(password),
+    role: 'client',
+    name,
+  });
+
+  try {
+    await newUser.save({ validateBeforeSave: true });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ message: 'Something went wrong, please check server logs' });
+  }
+
+  const { _id, role } = newUser;
+
+  const token = jwt.signToken(_id, email);
+
+  return res.status(200).json({
+    token,
+    user: {
+      email,
+      role,
+      name,
+    },
+  });
+};
