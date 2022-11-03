a0ff2fb2acbcee8767188b2ff88272a4db97df97
Gigiox <guillermo.casanova.b@gmail.com>
2022-11-03 16:50:44 -0300

    ✨ Created auth context

---
+++ context/auth/AuthProvider.tsx
@@ -0,0 +1,33 @@
+import { FC, useReducer } from 'react';
+import { IUser } from '../../interfaces';
+import { AuthContext, authReducer } from './';
+
+export interface AuthState {
+  isLoggedIn: boolean;
+  user?: IUser;
+}
+
+const AUTH_INITIAL_STATE: AuthState = {
+  isLoggedIn: false,
+  user: undefined,
+};
+
+export interface Props {
+  children: React.ReactNode;
+}
+
+export const AuthProvider: FC<Props> = ({ children }) => {
+  const [state, dispatch] = useReducer(authReducer, AUTH_INITIAL_STATE);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        ...state,
+
+        // Methods
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
