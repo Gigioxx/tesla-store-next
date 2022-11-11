2058236fc8ddb866843cbc29919bfc5b2df838f2
Gigiox <guillermo.casanova.b@gmail.com>
2022-11-11 19:31:35 -0300

    ✨ Using server side props to verify authentication

--- pages/checkout/address.tsx
+++ pages/checkout/address.tsx
@@ -1,3 +1,4 @@
+import { GetServerSideProps } from 'next';
 import {
   Box,
   Button,
@@ -9,6 +10,7 @@ import {
   Typography,
 } from '@mui/material';
 import { ShopLayout } from '../../components/layouts';
+import { jwt } from '../../utils';
 
 const AddressPage = () => {
   return (
@@ -63,6 +65,31 @@ const AddressPage = () => {
       </Box>
     </ShopLayout>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const { token = '' } = req.cookies;
+  let isValidToken = false;
+
+  try {
+    await jwt.isValidToken(token);
+    isValidToken = true;
+  } catch (error) {
+    isValidToken = false;
+  }
+
+  if (!isValidToken) {
+    return {
+      redirect: {
+        destination: '/auth/login?p=/checkout/address',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
 };
 
 export default AddressPage;
