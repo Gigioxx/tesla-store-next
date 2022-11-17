0b6cfb0898b105704d3018e57f7ffc244f9ed978
Gigiox <guillermo.casanova.b@gmail.com>
2022-11-17 20:38:59 -0300

    ✨ Using react hook form on address page and reading from cookies

--- pages/checkout/address.tsx
+++ pages/checkout/address.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { useRouter } from 'next/router';
 import {
   Box,
   Button,
@@ -9,87 +9,214 @@ import {
   TextField,
   Typography,
 } from '@mui/material';
+import Cookies from 'js-cookie';
+import { useForm } from 'react-hook-form';
 import { ShopLayout } from '../../components/layouts';
-import { jwt } from '../../utils';
+import { countries } from '../../utils';
+
+type FormData = {
+  firstName: string;
+  lastName: string;
+  address: string;
+  address2?: string;
+  zip: string;
+  city: string;
+  country: string;
+  phone: string;
+};
+
+const getAddressFromCookies = (): FormData => {
+  return {
+    firstName: Cookies.get('firstName') || '',
+    lastName: Cookies.get('lastName') || '',
+    address: Cookies.get('address') || '',
+    address2: Cookies.get('address2') || '',
+    zip: Cookies.get('zip') || '',
+    city: Cookies.get('city') || '',
+    country: Cookies.get('country') || '',
+    phone: Cookies.get('phone') || '',
+  };
+};
 
 const AddressPage = () => {
+  const router = useRouter();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    defaultValues: getAddressFromCookies(),
+  });
+
+  const onSubmitAddress = (data: FormData) => {
+    console.log(data);
+    Cookies.set('firstName', data.firstName);
+    Cookies.set('lastName', data.lastName);
+    Cookies.set('address', data.address);
+    Cookies.set('address2', data.address2 || '');
+    Cookies.set('zip', data.zip);
+    Cookies.set('city', data.city);
+    Cookies.set('country', data.country);
+    Cookies.set('phone', data.phone);
+
+    router.push('/checkout/summary');
+  };
+
   return (
     <ShopLayout
       title='Address'
       pageDescription='Confirm shipping and billing address'
     >
-      <Typography variant='h1' component='h1'>
-        Address
-      </Typography>
+      <form onSubmit={handleSubmit(onSubmitAddress)}>
+        <Typography variant='h1' component='h1'>
+          Address
+        </Typography>
 
-      <Grid container spacing={2} sx={{ mt: 2 }}>
-        <Grid item xs={12} sm={6}>
-          <TextField label='First name' variant='filled' fullWidth />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField label='Last name' variant='filled' fullWidth />
-        </Grid>
+        <Grid container spacing={2} sx={{ mt: 2 }}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              label='First name'
+              variant='filled'
+              fullWidth
+              {...register('firstName', {
+                required: 'First name is required',
+              })}
+              error={!!errors.firstName}
+              helperText={errors.firstName?.message}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              label='Last name'
+              variant='filled'
+              fullWidth
+              {...register('lastName', {
+                required: 'Last name is required',
+              })}
+              error={!!errors.lastName}
+              helperText={errors.lastName?.message}
+            />
+          </Grid>
 
-        <Grid item xs={12} sm={6}>
-          <TextField label='Address' variant='filled' fullWidth />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField label='Address 2 (Optional)' variant='filled' fullWidth />
-        </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              label='Address'
+              variant='filled'
+              fullWidth
+              {...register('address', {
+                required: 'Address is required',
+              })}
+              error={!!errors.address}
+              helperText={errors.address?.message}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              label='Address 2 (Optional)'
+              variant='filled'
+              fullWidth
+              {...register('address2')}
+            />
+          </Grid>
 
-        <Grid item xs={12} sm={6}>
-          <TextField label='Postal code' variant='filled' fullWidth />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField label='City' variant='filled' fullWidth />
-        </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              label='Zip code'
+              variant='filled'
+              fullWidth
+              {...register('zip', {
+                required: 'Zip code is required',
+              })}
+              error={!!errors.zip}
+              helperText={errors.zip?.message}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              label='City'
+              variant='filled'
+              fullWidth
+              {...register('city', {
+                required: 'City is required',
+              })}
+              error={!!errors.city}
+              helperText={errors.city?.message}
+            />
+          </Grid>
 
-        <Grid item xs={12} sm={6}>
-          <FormControl fullWidth>
-            <Select variant='filled' label='Country' value={1}>
-              <MenuItem value={1}>Chile</MenuItem>
-              <MenuItem value={2}>Argentina</MenuItem>
-              <MenuItem value={3}>United States</MenuItem>
-              <MenuItem value={4}>Canada</MenuItem>
-            </Select>
-          </FormControl>
+          <Grid item xs={12} sm={6}>
+            <FormControl fullWidth>
+              <TextField
+                select
+                variant='filled'
+                label='Country'
+                defaultValue={countries[0].code}
+                {...register('country', {
+                  required: 'Country is required',
+                })}
+                error={!!errors.country}
+                helperText={errors.country?.message}
+              >
+                {countries.map((country) => (
+                  <MenuItem key={country.code} value={country.code}>
+                    {country.name}
+                  </MenuItem>
+                ))}
+              </TextField>
+            </FormControl>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              label='Phone number'
+              variant='filled'
+              fullWidth
+              {...register('phone', {
+                required: 'Phone is required',
+              })}
+              error={!!errors.phone}
+              helperText={errors.phone?.message}
+            />
+          </Grid>
         </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField label='Phone number' variant='filled' fullWidth />
-        </Grid>
-      </Grid>
-      <Box sx={{ mt: 5 }} display='flex' justifyContent='center'>
-        <Button color='secondary' className='circular-btn' size='large'>
-          Check order
-        </Button>
-      </Box>
+        <Box sx={{ mt: 5 }} display='flex' justifyContent='center'>
+          <Button
+            type='submit'
+            color='secondary'
+            className='circular-btn'
+            size='large'
+          >
+            Check order
+          </Button>
+        </Box>
+      </form>
     </ShopLayout>
   );
 };
 
-export const getServerSideProps: GetServerSideProps = async ({ req }) => {
-  const { token = '' } = req.cookies;
-  let isValidToken = false;
+// export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+//   const { token = '' } = req.cookies;
+//   let isValidToken = false;
 
-  try {
-    await jwt.isValidToken(token);
-    isValidToken = true;
-  } catch (error) {
-    isValidToken = false;
-  }
+//   try {
+//     await jwt.isValidToken(token);
+//     isValidToken = true;
+//   } catch (error) {
+//     isValidToken = false;
+//   }
 
-  if (!isValidToken) {
-    return {
-      redirect: {
-        destination: '/auth/login?p=/checkout/address',
-        permanent: false,
-      },
-    };
-  }
+//   if (!isValidToken) {
+//     return {
+//       redirect: {
+//         destination: '/auth/login?p=/checkout/address',
+//         permanent: false,
+//       },
+//     };
+//   }
 
-  return {
-    props: {},
-  };
-};
+//   return {
+//     props: {},
+//   };
+// };
 
 export default AddressPage;
