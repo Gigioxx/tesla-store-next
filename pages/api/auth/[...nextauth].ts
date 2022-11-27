9be5f96de521ce161702198c1ced6d8c875213ed
Gigiox <guillermo.casanova.b@gmail.com>
2022-11-27 13:10:04 -0300

    ✨ Added custom login on nextAuth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,13 +1,39 @@
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import Credentials from 'next-auth/providers/credentials';
+
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
+    Credentials({
+      name: 'Custom Login',
+      credentials: {
+        email: {
+          label: 'Email:',
+          type: 'email',
+          placeholder: 'react@next.com',
+        },
+        password: {
+          label: 'Password:',
+          type: 'password',
+          placeholder: 'Password',
+        },
+      },
+      async authorize(credentials) {
+        console.log({ credentials });
+        // Todo: validate credentials with database values
+
+        return { name: 'John', email: 'john@google.com', role: 'admin' };
+      },
+    }),
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
     // ...add more providers here
   ],
+
+  // Callbacks
+  callbacks: {},
 };
 export default NextAuth(authOptions);
