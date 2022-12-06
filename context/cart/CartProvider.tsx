a0301fe3dd37245c4125621c7a8ed2f5c43f24bf
Guillermo Casanova <guillermo.casanova.b@gmail.com>
2022-12-05 23:17:17 -0300

    ✨ Created order interfaces

--- context/cart/CartProvider.tsx
+++ context/cart/CartProvider.tsx
@@ -1,7 +1,7 @@
 import { FC, useEffect, useReducer } from 'react';
 import Cookie from 'js-cookie';
 
-import { ICartProduct } from '../../interfaces';
+import { ICartProduct, ShippingAddress } from '../../interfaces';
 import { CartContext, cartReducer } from './';
 
 export interface CartState {
@@ -13,17 +13,6 @@ export interface CartState {
   total: number;
 
   shippingAddress?: ShippingAddress;
-}
-
-export interface ShippingAddress {
-  firstName: string;
-  lastName: string;
-  address: string;
-  address2?: string;
-  zip: string;
-  city: string;
-  country: string;
-  phone: string;
 }
 
 export const CART_INITIAL_STATE: CartState = {
