02d1145ac166a308fdce931a2cb8bdd0cfa40343
Guillermo Casanova <guillermo.casanova.b@gmail.com>
2022-12-06 18:09:02 -0300

    ✨ Validating order prices and creating new order in mongo

--- pages/api/orders/index.ts
+++ pages/api/orders/index.ts
@@ -1,8 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { getSession } from 'next-auth/react';
+import { db } from '../../../database';
+import { IOrder } from '../../../interfaces/order';
+import { Order, Product } from '../../../models';
 
-type Data = {
-  message: string;
-};
+type Data = { message: string } | IOrder;
 
 export default function handler(
   req: NextApiRequest,
@@ -17,7 +19,56 @@ export default function handler(
   }
 }
 const createOrder = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
-  const body = req.body;
+  const { orderItems, total } = req.body as IOrder;
 
-  return res.status(201).json(body);
+  // Verify that user exists
+  const session: any = await getSession({ req });
+
+  if (!session) {
+    return res.status(401).json({ message: 'Not authenticated' });
+  }
+
+  // Create array with order items
+  const productIds = orderItems.map((product) => product._id);
+  await db.connect();
+
+  const dbProducts = await Product.find({ _id: { $in: productIds } });
+
+  try {
+    const subTotal = orderItems.reduce((prev, current) => {
+      const currentPrice = dbProducts.find(
+        (prod) => prod.id === current._id
+      )!.price;
+      if (!currentPrice) {
+        throw new Error('Product not found');
+      }
+
+      return current.quantity * currentPrice + prev;
+    }, 0);
+
+    const taxRate = Number(process.env.NEXT_PUBLIC_TAX_RATE || 0);
+    const backendTotal = subTotal * (taxRate + 1);
+
+    if (total !== backendTotal) {
+      throw new Error('Total does not match');
+    }
+
+    const userId = session.user._id;
+    const newOrder = new Order({
+      ...req.body,
+      isPaid: false,
+      user: userId,
+    });
+    await newOrder.save();
+    await db.disconnect();
+
+    return res.status(201).json(newOrder);
+  } catch (error: any) {
+    await db.disconnect();
+    console.log(error);
+    res.status(400).json({
+      message:
+        error.message || 'Something went wrong, please check server logs',
+    });
+  }
 };
