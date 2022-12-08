c4664b796336d8e2949d1092d6c2d91bde570e6c
Guillermo Casanova <guillermo.casanova.b@gmail.com>
2022-12-08 11:50:09 -0300

    ✨ Using real value in paypal button

--- pages/orders/[id].tsx
+++ pages/orders/[id].tsx
@@ -116,7 +116,7 @@ const OrderPage: NextPage<Props> = ({ order }) => {
                         purchase_units: [
                           {
                             amount: {
-                              value: '2000.99',
+                              value: `${order.total}`,
                             },
                           },
                         ],
@@ -126,7 +126,7 @@ const OrderPage: NextPage<Props> = ({ order }) => {
                       return actions.order!.capture().then((details) => {
                         console.log(details);
                         const name = details.payer.name!.given_name;
-                        alert(`Transaction completed by ${name}`);
+                        // alert(`Transaction completed by ${name}`);
                       });
                     }}
                   />
