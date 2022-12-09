02fa6faae40e8abba7c4f808854c612af5301f8e
Guillermo Casanova <guillermo.casanova.b@gmail.com>
2022-12-09 17:10:18 -0300

    ✨ Updated dashboardPage, added stats cards

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
-import { DashboardOutlined } from '@mui/icons-material';
+import {
+  AccessTimeOutlined,
+  AttachMoneyOutlined,
+  CancelPresentationOutlined,
+  CategoryOutlined,
+  CreditCardOutlined,
+  DashboardOutlined,
+  GroupOutlined,
+  ProductionQuantityLimitsOutlined,
+} from '@mui/icons-material';
 import { AdminLayout } from '../../components/layouts';
+import { Grid } from '@mui/material';
+import { SummaryTile } from '../../components/admin';
+import { CreditCardOffOutlined } from '@mui/icons-material';
 
 const DashboardPage = () => {
   return (
@@ -9,7 +21,62 @@ const DashboardPage = () => {
       subtitle='General stats'
       icon={<DashboardOutlined />}
     >
-      <h3>Hello world!</h3>
+      <Grid container spacing={2}>
+        <SummaryTile
+          title={1}
+          subtitle='Total orders'
+          icon={<CreditCardOutlined color='secondary' sx={{ fontSize: 40 }} />}
+        />
+
+        <SummaryTile
+          title={2}
+          subtitle='Paid orders'
+          icon={<AttachMoneyOutlined color='success' sx={{ fontSize: 40 }} />}
+        />
+
+        <SummaryTile
+          title={3}
+          subtitle='Pending orders'
+          icon={<CreditCardOffOutlined color='error' sx={{ fontSize: 40 }} />}
+        />
+
+        <SummaryTile
+          title={4}
+          subtitle='Clients'
+          icon={<GroupOutlined color='primary' sx={{ fontSize: 40 }} />}
+        />
+
+        <SummaryTile
+          title={5}
+          subtitle='Products'
+          icon={<CategoryOutlined color='warning' sx={{ fontSize: 40 }} />}
+        />
+
+        <SummaryTile
+          title={6}
+          subtitle='Out of stock'
+          icon={
+            <CancelPresentationOutlined color='error' sx={{ fontSize: 40 }} />
+          }
+        />
+
+        <SummaryTile
+          title={7}
+          subtitle='Low stock'
+          icon={
+            <ProductionQuantityLimitsOutlined
+              color='warning'
+              sx={{ fontSize: 40 }}
+            />
+          }
+        />
+
+        <SummaryTile
+          title={8}
+          subtitle='Updating in...'
+          icon={<AccessTimeOutlined color='secondary' sx={{ fontSize: 40 }} />}
+        />
+      </Grid>
     </AdminLayout>
   );
 };
