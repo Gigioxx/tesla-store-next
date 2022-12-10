4acda6252475274064c599d987a03f5e4ba12f03
Guillermo Casanova <guillermo.casanova.b@gmail.com>
2022-12-10 18:18:40 -0300

    ✨ Added route protection for admin pages and endpoints

--- middleware.ts
+++ middleware.ts
@@ -3,19 +3,38 @@ import { getToken } from 'next-auth/jwt';
 // import * as jose from 'jose';
 
 export async function middleware(req: NextRequest) {
-  const session = await getToken({
+  const session: any = await getToken({
     req,
     secret: process.env.NEXTAUTH_SECRET,
   });
 
   if (!session) {
-    const { pathname } = req.nextUrl;
-    return NextResponse.redirect(new URL(`/auth/login?p=${pathname}`, req.url));
+    if (req.nextUrl.pathname.startsWith('/api/admin')) {
+      return NextResponse.redirect(new URL('/api/auth/unauthorized', req.url));
+    }
+
+    const requestedPage = req.nextUrl.pathname;
+    return NextResponse.redirect(
+      new URL(`/auth/login?p=${requestedPage}`, req.url)
+    );
+  }
+
+  const validRoles = ['admin'];
+  if (req.nextUrl.pathname.startsWith('/admin')) {
+    if (!validRoles.includes(session.user.role)) {
+      return NextResponse.redirect(new URL('/', req.url));
+    }
+  }
+
+  if (req.nextUrl.pathname.startsWith('/api/admin')) {
+    if (!validRoles.includes(session.user.role)) {
+      return NextResponse.redirect(new URL('/api/auth/unauthorized', req.url));
+    }
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/checkout/:path*'],
+  matcher: ['/checkout/:path*', '/admin/:path*', '/api/admin/:path*'],
 };
