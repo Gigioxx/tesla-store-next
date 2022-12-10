57776a99068450293a4f34bf87aac6b3b6473d56
Guillermo Casanova <guillermo.casanova.b@gmail.com>
2022-12-10 19:48:28 -0300

    ✨ Updating users roles and displaying them in real time

--- pages/admin/users.tsx
+++ pages/admin/users.tsx
@@ -1,24 +1,75 @@
+import { useState, useEffect } from 'react';
 import { PeopleOutline } from '@mui/icons-material';
-import { AdminLayout } from '../../components/layouts';
+import useSWR from 'swr';
 
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
-import { Grid } from '@mui/material';
-import useSWR from 'swr';
+import { Grid, MenuItem, Select } from '@mui/material';
+
+import { AdminLayout } from '../../components/layouts';
 import { IUser } from '../../interfaces';
+import teslaApi from '../../api/teslaApi';
 
 const UsersPage = () => {
   const { data, error } = useSWR<IUser[]>('/api/admin/users');
+  const [users, setUsers] = useState<IUser[]>([]);
+
+  useEffect(() => {
+    if (data) {
+      setUsers(data);
+    }
+  }, [data]);
 
   if (!data) return;
   if (!data && !error) return <div>Loading...</div>;
 
+  const onRoleUpdate = async (userId: string, newRole: string) => {
+    const previousUsers = users.map((user) => ({ ...user }));
+
+    const updatedUsers = users.map((user) => ({
+      ...user,
+      role: userId === user._id ? newRole : user.role,
+    }));
+
+    setUsers(updatedUsers);
+
+    try {
+      await teslaApi.put(`/admin/users`, {
+        userId,
+        role: newRole,
+      });
+    } catch (error) {
+      setUsers(previousUsers);
+      console.log(error);
+      alert('Error updating user role');
+    }
+  };
+
   const columns: GridColDef[] = [
     { field: 'email', headerName: 'Email', width: 250 },
     { field: 'name', headerName: 'Full name', width: 300 },
-    { field: 'role', headerName: 'Role', width: 300 },
+    {
+      field: 'role',
+      headerName: 'Role',
+      width: 300,
+      renderCell: ({ row }) => {
+        return (
+          <Select
+            value={row.role}
+            label='Role'
+            onChange={({ target }) => onRoleUpdate(row.id, target.value)}
+            sx={{ width: 300 }}
+          >
+            <MenuItem value='admin'>Admin</MenuItem>
+            <MenuItem value='client'>Client</MenuItem>
+            <MenuItem value='super-user'>Super User</MenuItem>
+            <MenuItem value='SEO'>SEO</MenuItem>
+          </Select>
+        );
+      },
+    },
   ];
 
-  const rows = data!.map((user) => ({
+  const rows = users.map((user) => ({
     id: user._id,
     email: user.email,
     name: user.name,
