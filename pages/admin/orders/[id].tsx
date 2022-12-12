6b93a5dc6d48617c899f2029a6682b50e17254cb
Guillermo Casanova <guillermo.casanova.b@gmail.com>
2022-12-11 21:47:02 -0300

    ✨ Created admin order page view

---
+++ pages/admin/orders/[id].tsx
@@ -0,0 +1,153 @@
+import { GetServerSideProps, NextPage } from 'next';
+
+import {
+  Box,
+  Card,
+  CardContent,
+  Chip,
+  Divider,
+  Grid,
+  Typography,
+} from '@mui/material';
+import { CartList, OrderSummary } from '../../../components/cart';
+import {
+  ConfirmationNumberOutlined,
+  CreditCardOffOutlined,
+  CreditScoreOutlined,
+} from '@mui/icons-material';
+import { dbOrders } from '../../../database';
+import { IOrder } from '../../../interfaces';
+import { countries } from '../../../utils';
+import { AdminLayout } from '../../../components/layouts';
+
+interface Props {
+  order: IOrder;
+}
+
+const AdminOrderPage: NextPage<Props> = ({ order }) => {
+  const { shippingAddress } = order;
+
+  const countryName = countries.filter(
+    (item) => item.code === shippingAddress.country
+  )[0].name;
+
+  return (
+    <AdminLayout
+      title='Order summary'
+      subtitle={`OrderId: ${order._id}`}
+      icon={<ConfirmationNumberOutlined />}
+    >
+      {order.isPaid ? (
+        <Chip
+          sx={{ my: 2 }}
+          label='Payment successful'
+          variant='outlined'
+          color='success'
+          icon={<CreditScoreOutlined />}
+        />
+      ) : (
+        <Chip
+          sx={{ my: 2 }}
+          label='Pending payment'
+          variant='outlined'
+          color='error'
+          icon={<CreditCardOffOutlined />}
+        />
+      )}
+
+      <Grid container className='fadeIn'>
+        <Grid item xs={12} sm={7}>
+          <CartList products={order.orderItems} />
+        </Grid>
+        <Grid item xs={12} sm={5}>
+          <Card className='summary-card'>
+            <CardContent>
+              <Typography variant='h2'>
+                Summary ({order.numberOfItems}{' '}
+                {order.numberOfItems > 1 ? 'Items' : 'Item'})
+              </Typography>
+              <Divider sx={{ my: 1 }} />
+
+              <Box display='flex' justifyContent='space-between'>
+                <Typography variant='subtitle1'>Shipping address</Typography>
+              </Box>
+
+              <Typography>
+                {shippingAddress.firstName} {shippingAddress.lastName}
+              </Typography>
+              <Typography>
+                {shippingAddress.address}{' '}
+                {shippingAddress.address2
+                  ? `, ${shippingAddress.address2}`
+                  : ''}
+              </Typography>
+              <Typography>
+                {shippingAddress.city} {shippingAddress.zip}
+              </Typography>
+              <Typography>{countryName}</Typography>
+              <Typography>{shippingAddress.phone}</Typography>
+
+              <Divider sx={{ my: 1 }} />
+
+              <OrderSummary
+                orderValues={{
+                  numberOfItems: order.numberOfItems,
+                  subTotal: order.subTotal,
+                  total: order.total,
+                  tax: order.tax,
+                }}
+              />
+
+              <Box sx={{ mt: 3 }} display='flex' flexDirection='column'>
+                <Box display='flex' flexDirection='column'>
+                  {order.isPaid ? (
+                    <Chip
+                      sx={{ my: 2, flex: 1 }}
+                      label='Payment successful'
+                      variant='outlined'
+                      color='success'
+                      icon={<CreditScoreOutlined />}
+                    />
+                  ) : (
+                    <Chip
+                      sx={{ my: 2, flex: 1 }}
+                      label='Pending payment'
+                      variant='outlined'
+                      color='error'
+                      icon={<CreditCardOffOutlined />}
+                    />
+                  )}
+                </Box>
+              </Box>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </AdminLayout>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async ({
+  req,
+  query,
+}) => {
+  const { id = '' } = query;
+  const order = await dbOrders.getOrderById(id.toString());
+
+  if (!order) {
+    return {
+      redirect: {
+        destination: `/admin/orders`,
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      order,
+    },
+  };
+};
+
+export default AdminOrderPage;
