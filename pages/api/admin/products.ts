97aeddefb86d05a330d820e8df629a8ab7bb9a98
Guillermo Casanova <guillermo.casanova.b@gmail.com>
2022-12-14 20:41:59 -0300

    ✨ Added endpoint to update products

--- pages/api/admin/products.ts
+++ pages/api/admin/products.ts
@@ -1,9 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { IProduct } from '../../../interfaces';
+import { isValidObjectId } from 'mongoose';
 import { db } from '../../../database';
+import { IProduct } from '../../../interfaces';
 import { Product } from '../../../models';
 
-type Data = { message: string } | IProduct[];
+type Data = { message: string } | IProduct[] | IProduct;
 
 export default function handler(
   req: NextApiRequest,
@@ -14,6 +15,7 @@ export default function handler(
       return getProducts(req, res);
 
     case 'PUT':
+      return updateProduct(req, res);
     case 'POST':
 
     default:
@@ -34,4 +36,41 @@ const getProducts = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   // Update images
 
   return res.status(200).json(products);
+};
+
+const updateProduct = async (
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) => {
+  const { _id = '', images = [] } = req.body as IProduct;
+
+  if (!isValidObjectId(_id)) {
+    return res.status(400).json({ message: 'Invalid product id' });
+  }
+
+  if (images.length < 2) {
+    return res.status(400).json({ message: 'At least 2 images are required' });
+  }
+
+  try {
+    await db.connect();
+    const product = await Product.findById(_id);
+    if (!product) {
+      await db.disconnect();
+      return res.status(400).json({ message: 'Product not found' });
+    }
+
+    // todo: delete cloudinary images
+
+    await product.update(req.body);
+    await db.disconnect();
+
+    return res.status(200).json(product);
+  } catch (error) {
+    console.log(error);
+    await db.disconnect();
+    return res
+      .status(400)
+      .json({ message: 'Something went wrong, please check server console' });
+  }
 };
